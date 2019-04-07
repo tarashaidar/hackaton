01cd21f91eeb80c6685220c4a8b79be037c21271
Taras <47187659+tarashaidar@users.noreply.github.com>
2019-04-07 19:02:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const carsCoords = [
+  {
+    lat: 49.843115415548205,
+    lng: 24.02199078482152
+  },
+  {
+    lat: 49.84440230289024,
+    lng: 24.025531300721177
+  },
+  {
+    lat: 49.83989112801838,
+    lng: 24.029286393342026
+  },
+  {
+    lat: 49.84177314171475,
+    lng: 24.024308213410386
+  }
+];
+
 window.initMap = function () {
   getLocation(function (error, pos) {
       if (error) {
@@ -16,18 +35,28 @@ window.initMap = function () {
       const image = 'https://raw.githubusercontent.com/SergiySobchuk/www/master/uploads_images/passanger_54_54.png';
       const map = new google.maps.Map(document.getElementById('map'), {zoom: 16, center: uluru});
       var marker = new google.maps.Marker({position: uluru, map: map, icon: image});
-
+      
+      carsCoords.forEach(coordinate => {
+        const carImage = new google.maps.MarkerImage(
+          "https://raw.githubusercontent.com/SergiySobchuk/www/master/uploads_images/taxi.png",
+          new google.maps.Size(45, 45),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(-22, 0)
+        )
+        const carPosition = new google.maps.Marker({position: coordinate, map: map, icon: carImage, draggable: true});
+      });
       const newMarker = new google.maps.Marker({position: uluru, map: map, icon: pointPosition, draggable: false, navigationContol: true});
 
-      google.maps.event.addListener(map, "dragend", function() {
+      window.google.maps.event.addListener(map, "dragend", function() {
         console.log(newMarker.getPosition().lat());
         console.log(newMarker.getPosition().lng());
       });
-      window.google.maps.event.addListener(map , 'drag', function (event) {
+      window.google.maps.event.addListener(map , 'drag', function () {
         newMarker.setPosition( map.getCenter() );
     });
   });
 }
+
 
 function getLocation(callback) {
   if (navigator.geolocation) {
